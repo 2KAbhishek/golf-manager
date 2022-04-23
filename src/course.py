1249f20893fca682ac3e64800feef9e18b7cbe71
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-24 00:56:34 +0530

    Add course class

---
+++ src/course.py
@@ -0,0 +1,40 @@
+import datetime
+from hole import Hole
+
+
+class Course:
+    def __init__(self, filename):
+        self._name = filename.split(".")[0]
+        self._holes = []
+        self._totalPar = 0
+        self._readFile(filename)
+
+    def _readFile(self, filename):
+        with open(filename, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                line = line.strip()
+                if line:
+                    self._holes.append(Hole(*line.split()))
+                    self._totalPar += int(line.split()[1])
+
+    @property
+    def name(self):
+        return self._name
+
+    def getPlaySchedule(self, teeTime):
+        print("Tee Off Time: {}".format(teeTime))
+        print("Course: {} \t\t Total PAR: {}".format(self._name, self._totalPar))
+        print("Hole\t PAR\t Index\t Distance\t Start\t Finish")
+        for i, hole in enumerate(self._holes):
+            print("{}\t {}\t {}\t {}\t {}\t {}".format(i+1, hole.getPar(), hole.getIndex(),
+                  hole.getDistance(), teeTime, teeTime + datetime.timedelta(seconds=hole.getDuration())))
+
+    def __str__(self):
+        out = "Course: {} \t\t Total PAR: {}\n".format(
+            self._name, self._totalPar)
+        out += "Hole\t PAR\t Index\t Distance\n"
+        for i, hole in enumerate(self._holes):
+            out += "{}\t {}\t {}\t {}\n".format(
+                i+1, hole.getPar(), hole.getIndex(), hole.getDistance())
+        return out
