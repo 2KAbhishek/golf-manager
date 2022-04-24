436f648b19ae3a8d39306127993ef61d96832dc2
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-24 15:40:02 +0530

    Update constructor logic

--- src/flight.py
+++ src/flight.py
@@ -3,10 +3,12 @@ from golfing_exception import GolfingException
 class Flight:
     def __init__(self, golfers):
         if len(golfers) < 3 or len(golfers) > 4:
-            raise GolfingException("Flight can only consists of 3 or 4 golfers")
+            raise GolfingException(
+                "Flight can only consists of 3 or 4 golfers")
         for golfer in golfers:
-            if golfer.membership != "Full" or golfer.getMembershipStatus() == False:
-                raise GolfingException(golfer.name + " has inactive membership")
+            if golfer.getMembershipStatus() == False:
+                raise GolfingException("Golfer {} {} has inactive membership"
+                                       .format(golfer.memberID, golfer.name))
         self._golfers = golfers
 
     def searchGolfer(self, memberID):
