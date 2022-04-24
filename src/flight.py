f170a293b09f20aebc36f334b4121c5cc679d535
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-24 15:40:11 +0530

    Add main method

--- src/flight.py
+++ src/flight.py
@@ -1,4 +1,8 @@
+import datetime
 from golfing_exception import GolfingException
+from pc_holder import PCHolder
+from handicapped_golfer import HandicappedGolfer
+
 
 class Flight:
     def __init__(self, golfers):
@@ -25,3 +29,29 @@ class Flight:
             if golfer.membership != "Full" or golfer.getMembershipStatus() == False:
                 return False
         return True
+
+
+if __name__ == "__main__":
+    golfers1 = [
+        HandicappedGolfer("Jeff", "Full", 13.1),
+        HandicappedGolfer("Jim", "Basic", 4),
+        HandicappedGolfer("Joe", "Full", 19),
+        HandicappedGolfer("Jack", "Full", 2.3),
+    ]
+    flight1 = Flight(golfers1)
+    basicGolfer = flight1.searchGolfer(2)
+    basicGolfer.membership = "Full"
+    print("Flight1 Weekend eligibility {}".format(flight1.getWeekendEligibility()))
+
+    golfers2 = [
+        HandicappedGolfer("Tom", "Full", 11),
+        HandicappedGolfer("Neil", "Full", 2.5),
+        PCHolder("Charles", "Full", datetime.datetime(2021, 7, 31))
+    ]
+    try:
+        flight2 = Flight(golfers2)
+    except GolfingException as e:
+        renewedGolfer = golfers2[2]
+        renewedGolfer.renew(datetime.datetime(2032, 7, 31))
+        flight2 = Flight(golfers2)
+    print("Flight2 Weekend eligibility {}".format(flight2.getWeekendEligibility()))
