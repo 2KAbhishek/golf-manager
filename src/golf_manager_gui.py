a907892ca641531ec59240b144f5a3721a53c120
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-26 04:31:12 +0530

    Add showSchedule method

---
+++ src/golf_manager_gui.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import scrolledtext
+import tkinter.font as tkFont
+from course import Course
+
+
+def showSchedule():
+    try:
+        course_obj = Course("../data/" + courseName.get().strip())
+        schedule = course_obj.getScheduleStr(teeTimeEntry.get().strip())
+        scroll.delete(1.0, END)
+        scroll.insert(INSERT, schedule)
+    except Exception as e:
+        print(e)
+        scroll.delete(1.0, END)
+        scroll.insert(INSERT, "Please enter valid input")
+
+
