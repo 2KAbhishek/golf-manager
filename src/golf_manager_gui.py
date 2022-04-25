e7a963f9b8f3d37051087924b824aea934eb51bd
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-26 04:32:40 +0530

    Add course selection radio button

--- src/golf_manager_gui.py
+++ src/golf_manager_gui.py
@@ -35,5 +35,23 @@ if __name__ == "__main__":
     teeTimeEntry.grid(row=0, column=1, padx=10, pady=10)
     teeTimeEntry.insert(0, "07:08")
 
+    # Create a label
+    label = Label(root, text="Course:")
+    label.grid(row=1, column=0, padx=10, pady=10)
+
+    # Create a radio button
+    courseName = StringVar()
+    courseName.set("Augusta.txt")
+    R1 = Radiobutton(root, text="Augusta", variable=courseName,
+                     value="Augusta.txt")
+    R1.grid(row=1, column=1, padx=10, pady=10)
+
+    R2 = Radiobutton(root, text="Laguna", variable=courseName,
+                     value="Laguna.txt")
+    R2.grid(row=2, column=1, padx=10, pady=10)
+
+    R3 = Radiobutton(root, text="Pebble Bay", variable=courseName,
+                     value="Pebble_Bay.txt")
+    R3.grid(row=3, column=1, padx=10, pady=10)
 
     root.mainloop()
